73f25e809299667e9a80a9c3fe0ccceff1821c1a
Toky RAFIDIMANANA <rafidimananatokynyaina@gmail.com>
2024-04-28 15:44:21 +0300

    Initiate project

---
+++ src/main/java/mg/toky/MAP_SI/MapSiApplication.java
@@ -0,0 +1,13 @@
+package mg.toky.MAP_SI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapSiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/toky/MAP_SI/MapSiApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.toky.MAP_SI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapSiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
